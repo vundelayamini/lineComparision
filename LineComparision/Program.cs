ca5c0b44b0a111489a152f672bd04634fcc978c3
vundelayamini <vundelayamini@gmail.com>
2021-03-09 20:11:28 +0530

    [yamini]ADD: compare Two lines

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 
-namespace GreaterOrLess
+namespace LineComparision
 {
     class Program
     {
         static void Main(string[] args)
         {
             int x1, x2, y1, y2, x3, y3, x4, y4;
-            Console.WriteLine("Pass The values x1 & y1");
+            Console.WriteLine("Enter values for x1 & y1");
             x1 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Pass The values of x2 & y2");
+            Console.WriteLine("Enter values for x2 & y2");
             x2 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Pass The values x3 & y3 for Second Line");
+            Console.WriteLine("Enter values for x3 & y3 for Line 2");
             x3 = Convert.ToInt32(Console.ReadLine());
             y3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Pass The values x4 & y4 for Second Line");
+            Console.WriteLine("Enter values for x4 & y4 for Line 2");
             x4 = Convert.ToInt32(Console.ReadLine());
             y4 = Convert.ToInt32(Console.ReadLine());
-            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            int diffrence = length1.CompareTo(length2);
-            if (diffrence == 0)
+            double line1 = (Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2));
+            double line2 = (Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2));
+            int variation = line1.CompareTo(line2);
+            Boolean difference2 = line1.Equals(line2);
+            if (difference2 == true)
                 Console.WriteLine("Both Lines are equal in length");
-            else if (diffrence > 0)
+            else
+                Console.WriteLine("Both are not equal in length");
+
+            if (difference == 0)
+                Console.WriteLine("Both Lines are equal in length");
+            else if (difference > 0)
                 Console.WriteLine("First Line is greater than Second Line");
             else
                 Console.WriteLine("First Line is smaller than Second Line");
